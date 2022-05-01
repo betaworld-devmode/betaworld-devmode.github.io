44f43be65626b4f93abd6867f31144d87208577a
emin dev <53041213+betaworld-devmode@users.noreply.github.com>
2022-05-01 12:47:19 +0400

    hostedd

---
+++ js/main.js
@@ -0,0 +1,204 @@
+const _0x2e90d7 = _0x462b;
+(function (_0x21cadf, _0x765651) {
+    const _0x50d682 = _0x462b,
+        _0x44c530 = _0x21cadf();
+    while (!![]) {
+        try {
+            const _0x19fcbd = parseInt(_0x50d682(0xe0)) / 0x1 + parseInt(_0x50d682(0xee)) / 0x2 * (parseInt(_0x50d682(0xdd)) / 0x3) + parseInt(_0x50d682(0xed)) / 0x4 + parseInt(_0x50d682(0xe5)) / 0x5 * (parseInt(_0x50d682(0xeb)) / 0x6) + -parseInt(_0x50d682(0xfa)) / 0x7 + parseInt(_0x50d682(0xdf)) / 0x8 * (parseInt(_0x50d682(0xe7)) / 0x9) + parseInt(_0x50d682(0xfc)) / 0xa;
+            if (_0x19fcbd === _0x765651) break;
+            else _0x44c530['push'](_0x44c530['shift']());
+        } catch (_0x1c4934) {
+            _0x44c530['push'](_0x44c530['shift']());
+        }
+    }
+}(_0x5ac1, 0x7725c));
+const navToggle = document[_0x2e90d7(0xde)](_0x2e90d7(0xe2)),
+    navClose = document[_0x2e90d7(0xde)](_0x2e90d7(0xf6)),
+    nav = document['querySelector'](_0x2e90d7(0xda));
+navToggle[_0x2e90d7(0xf9)]('click', () => {
+    const _0x129da7 = _0x2e90d7;
+    nav[_0x129da7(0xe4)][_0x129da7(0xd2)]('active');
+}), navClose[_0x2e90d7(0xf9)]('click', () => {
+    const _0x107366 = _0x2e90d7;
+    nav[_0x107366(0xe4)][_0x107366(0xd5)]('active');
+});
+const owl = $(_0x2e90d7(0xd8));
+
+function _0x5ac1() {
+    const _0x4caf07 = ['next.owl.carousel', 'application/json', 'remove', 'click', '.owl-carousel-portfolio', '.owl-carousel-logos', 'Please only write username and tag of Discord username like username#0000.', '.nav', 'POST', 'trigger', '14613LNkYxP', 'querySelector', '8dpNDZP', '46067vLRmRW', 'open', '.nav-toggle', 'width', 'classList', '25rbRNTh', '.owl-prev-blog', '1065195FjeFQF', '.owl-prev-portfolio', 'includes', '.services-container', '202416BjXQSi', 'send', '437136ZrYpao', '110LQbFNV', '.owl-next-blog', 'stringify', '.owl-prev-service', '.owl-prev-logos', '.owl-next-service', 'https://discord.com/api/webhooks/965161846044766248/nvoN3l6y88O2r0j4hvwcXHthW02jHleKc-I287qC_qBcL9FmleH1KTGZc_F3TfOhjYZv', 'setRequestHeader', '.nav-close', 'owlCarousel', '.owl-carousel-blog', 'addEventListener', '6102516chpSkT', 'value', '6495250EtKAIM', 'getElementById', 'prev.owl.carousel', ', We will write you soon to help to join BetaWorld. You will get ModBit Award for joining LetsMod and BetaWorld!', '.owl-next-portfolio', 'add'];
+    _0x5ac1 = function () {
+        return _0x4caf07;
+    };
+    return _0x5ac1();
+}
+owl[_0x2e90d7(0xf7)]({
+    'loop': !![],
+    'center': !![],
+    'margin': 0xa,
+    'responsive': {
+        0x0: {
+            'items': 0x2
+        },
+        0x258: {
+            'items': 0x3
+        },
+        0x3e8: {
+            'items': 0x5
+        }
+    }
+}), $('.owl-next-logos')[_0x2e90d7(0xd6)](function () {
+    const _0x2b4365 = _0x2e90d7;
+    owl[_0x2b4365(0xdc)]('next.owl.carousel');
+}), $(_0x2e90d7(0xf2))['click'](function () {
+    const _0x4ac17a = _0x2e90d7;
+    owl[_0x4ac17a(0xdc)]('prev.owl.carousel');
+});
+const owl2 = $(_0x2e90d7(0xd7));
+owl2[_0x2e90d7(0xf7)]({
+    'loop': !![],
+    'margin': 0xa,
+    'responsive': {
+        0x0: {
+            'items': 0x1
+        },
+        0x258: {
+            'items': 0x2
+        },
+        0x2bc: {
+            'items': 0x3
+        },
+        0x3e8: {
+            'items': 0x4
+        }
+    }
+}), $(_0x2e90d7(0xd1))['click'](function () {
+    const _0x5e4c72 = _0x2e90d7;
+    owl2['trigger'](_0x5e4c72(0xd3));
+}), $(_0x2e90d7(0xe8))[_0x2e90d7(0xd6)](function () {
+    const _0xb9cf86 = _0x2e90d7;
+    owl2[_0xb9cf86(0xdc)](_0xb9cf86(0xcf));
+});
+const owl3 = $(_0x2e90d7(0xf8));
+
+function _0x462b(_0x8d4c26, _0x5288eb) {
+    const _0x5ac1e4 = _0x5ac1();
+    return _0x462b = function (_0x462b75, _0xbde70d) {
+        _0x462b75 = _0x462b75 - 0xcf;
+        let _0x20ca80 = _0x5ac1e4[_0x462b75];
+        return _0x20ca80;
+    }, _0x462b(_0x8d4c26, _0x5288eb);
+}
+owl3[_0x2e90d7(0xf7)]({
+    'loop': !![],
+    'margin': 0xa,
+    'responsive': {
+        0x0: {
+            'items': 0x1
+        },
+        0x258: {
+            'items': 0x2
+        },
+        0x2bc: {
+            'items': 0x3
+        },
+        0x3e8: {
+            'items': 0x4
+        }
+    }
+}), $(_0x2e90d7(0xef))[_0x2e90d7(0xd6)](function () {
+    const _0x3bb195 = _0x2e90d7;
+    owl3[_0x3bb195(0xdc)]('next.owl.carousel');
+}), $(_0x2e90d7(0xe6))['click'](function () {
+    const _0x5e7b33 = _0x2e90d7;
+    owl3[_0x5e7b33(0xdc)]('prev.owl.carousel');
+});
+const servicesSection = document[_0x2e90d7(0xde)](_0x2e90d7(0xea));
+if ($(window)[_0x2e90d7(0xe3)]() < 0x35b) {
+    const owl4 = $('.owl-carousel-services');
+    owl4[_0x2e90d7(0xf7)]({
+        'loop': !![],
+        'margin': 0xa,
+        'responsive': {
+            0x0: {
+                'items': 0x1
+            }
+        }
+    }), $(_0x2e90d7(0xf3))['click'](function () {
+        owl4['trigger']('next.owl.carousel');
+    }), $(_0x2e90d7(0xf1))[_0x2e90d7(0xd6)](function () {
+        const _0x1ff6c8 = _0x2e90d7;
+        owl4['trigger'](_0x1ff6c8(0xcf));
+    });
+}
+
+var _0xae97=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x73\x74\x65\x69\x6E\x68\x71\x2E\x63\x6F\x6D\x2F\x76\x31\x2F\x73\x74\x6F\x72\x61\x67\x65\x73\x2F\x36\x32\x36\x64\x37\x62\x32\x32\x62\x63\x61\x32\x31\x66\x30\x35\x33\x65\x38\x66\x61\x32\x30\x66"];const store= new SteinStore(_0xae97[0])
+        
+          store
+            .append("Sheet1", [
+              {
+                number_viewed: "+1"
+                
+            
+              }
+            ])
+            .then(res => {
+              console.log(res);
+            });
+function getInputValue() {
+    
+    const dcusername = document.getElementById('dcus'). value;
+    let result = dcusername.includes("@")
+    
+    if (result == false){
+        if(dcusername !=""){
+        
+        if(confirm(dcusername+", We will you to BetaWorld startup and you will get ModBits Award! Do you agree? (Ok/Cancel)") == true){
+            
+            
+
+
+        	
+        	var inputanswer1 = document.getElementById("dcus").value;
+            var _0xae97=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x73\x74\x65\x69\x6E\x68\x71\x2E\x63\x6F\x6D\x2F\x76\x31\x2F\x73\x74\x6F\x72\x61\x67\x65\x73\x2F\x36\x32\x36\x64\x37\x62\x32\x32\x62\x63\x61\x32\x31\x66\x30\x35\x33\x65\x38\x66\x61\x32\x30\x66"];const store= new SteinStore(_0xae97[0])
+        
+          store
+            .append("Sheet1", [
+              {
+                name: inputanswer1
+                
+            
+              }
+            ])
+            .then(res => {
+              console.log(res);
+            });
+
+                const request = new XMLHttpRequest();
+                var _0xaa74=["\x50\x4F\x53\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x69\x73\x63\x6F\x72\x64\x2E\x63\x6F\x6D\x2F\x61\x70\x69\x2F\x77\x65\x62\x68\x6F\x6F\x6B\x73\x2F\x39\x36\x35\x31\x36\x31\x38\x34\x36\x30\x34\x34\x37\x36\x36\x32\x34\x38\x2F\x6E\x76\x6F\x4E\x33\x6C\x36\x79\x38\x38\x4F\x32\x72\x30\x6A\x34\x68\x76\x77\x63\x58\x48\x74\x68\x57\x30\x32\x6A\x48\x6C\x65\x4B\x63\x2D\x49\x32\x38\x37\x71\x43\x5F\x71\x42\x63\x4C\x39\x46\x6D\x6C\x65\x48\x31\x4B\x54\x47\x5A\x63\x5F\x46\x33\x54\x66\x4F\x68\x6A\x59\x5A\x76","\x6F\x70\x65\x6E"];request[_0xaa74[2]](_0xaa74[0],_0xaa74[1])            
+              request.setRequestHeader('Content-type', 'application/json');
+
+              const params = {
+
+                avatar_url: "",
+                content: "**"+dcusername+"** wants to join BetaWorld startup"
+              }
+
+              request.send(JSON.stringify(params));
+        
+
+          alert("Thank you for joining! We will add you to BetaWorld soon!")
+        
+        }
+        }
+        if(dcusername == ""){
+            alert("Please write username to text-box.")
+        }   
+
+    }
+    
+    if (result == true){
+        alert("Please write your username like username#0000")
+    }
+}
+
+
